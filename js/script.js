b930e10a5c471e0a6d46363deaba22655833c8c9
ricardoszanata <ricardoszanata@gmail.com>
2026-06-22 07:44:02 -0300

    Introdução ao Javascript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//function = função ou procedure do pascal
+//{ = ao begin
+//} = ao end
+//let = var
+function calcular(){
+    let num1 = document.getElementById('edtnum1').value;
+    let num2 = document.getElementById('edtnum2').value;
+    let soma = num1+num2;
+    console.log('Soma '+soma);
+    let sub = num1-num2;
+    console.log('Subtração '+sub);
+    let div = num1/num2;
+    console.log('Divisão '+div);
+    let mult = num1*num2;
+    console.log('Multiplicação '+mult);
+}
